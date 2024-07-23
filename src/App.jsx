b2987a831cb24412d021eb27a2f25e496f58e702
Shaily Macwan <shailymacwan12@gmail.com>
2024-07-23 02:25:42 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import ToDos from './Components/ToDos'
+
+function App() {
+
+  return (
+    <>
+      <ToDos />
+    </>
+  )
+}
+
+export default App
